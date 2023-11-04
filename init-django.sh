b657aea4da9f4fb5cad148fa66b8f79168cec550
Abenezer Haymanot <143000984+exptDevAb@users.noreply.github.com>
2023-11-04 06:54:37 -0400

    Create init-django.sh

---
+++ init-django.sh
@@ -0,0 +1,38 @@
+#!/bin/env bash
+
+pip install --upgrade django
+
+django_version=$(python -m django --version)
+
+echo "Django version $django_version has been installed."
+
+# Create a new Django project
+django-admin startproject demo_project
+
+# Change directory to the project folder
+cd demo_project
+
+folder_array=("blog" "css" "main")
+
+# Loop through each folder name and create Django app
+for folder_name in "${folder_array[@]}"
+do
+    python3 manage.py startapp "$folder_name"
+
+    # Change directory to the created folder
+    cd "$folder_name"
+
+    # Create urls.py file with the specified content
+    cat << EOF > urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('path-they-want/', views.members, name=''),
+]
+EOF
+
+    cd ..
+done
+
+echo "Demo project with starter apps has been created."
